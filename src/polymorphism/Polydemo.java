c9c947ee0b4cc19896a074c8386b3397e374ec6c
vaibhav nexttech <vn202207@nexttechvision.com>
2022-09-07 11:42:05 +0530

    Polymorphism

---
+++ src/polymorphism/Polydemo.java
@@ -0,0 +1,32 @@
+package polymorphism;
+
+import java.io.FileOutputStream;
+
+//polymorphism -> the ability to take one form to many form
+//or
+//one thing have many actions
+public class Polydemo {
+
+    /*
+        method overloading => a class with multiple functions with same name but different number of arguments
+
+        rule-1. changing number of arguments
+             2. changing the function datatype.
+
+         advantage -> it increase the readability of program
+
+     */
+    private int add(int a,int b){
+        return a+b;
+    }
+
+    private int add(int a,int b,int c){
+        return a+b+c;
+    }
+
+    public static void main(String[] args) {
+        Polydemo obj = new Polydemo();
+        System.out.println(obj.add(5,6));
+
+    }
+}
