43e6d46f3a2c0f037ba7c53dac2c4d72b8d20e69
vaibhav nexttech <vn202207@nexttechvision.com>
2022-09-26 11:43:51 +0530

    static keyword

--- src/S.java
+++
@@ -1,8 +0,0 @@
-public class S {
-    public static void main(String[] args) {
-
-        String str = "25165824.0";
-        Float fl = Float.parseFloat(str);
-        System.out.println((fl/1024)/1024);
-    }
-}
