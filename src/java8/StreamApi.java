dffb73cf7552d36249dfb78e4a020d349db75e75
Vaibhav N <vn202207@nexttechvision.com>
2022-11-30 11:57:25 +0530

    StreamApi

--- src/java8/StreamApi.java
+++ src/java8/StreamApi.java
@@ -1,6 +1,10 @@
 package java8;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
 
 /**
  * stream api = it is sequence of object and it have many methods
@@ -17,12 +21,37 @@ import java.util.function.Function;
 public class StreamApi {
     public static void main(String[] args) {
 
-        Function<Integer,Integer> fun = (integer)-> integer*integer;
+        Function<Integer, Integer> fun = (integer) -> integer * integer;
 
-        System.out.println(fun.apply(9));
+//        System.out.println(fun.apply(9));
 
-        Function<String,Integer> str = (st)->st.length();
-        System.out.println(str.apply("welcome"));
+        Function<String, Integer> str = (st) -> st.length();
+//        System.out.println(str.apply("welcome"));
+
+
+        List<Integer> list = Arrays.asList(1,2,3,45,6);
+
+        int max =0;
+
+        for (int i=0;i<list.size();i++){
+
+            if(list.get(i)>max){
+                max = list.get(i);
+            }
+        }
+
+        System.out.println("max-->"+max);
+        System.out.println(IntStream.of(1, 2, 3, 4, 5, 6, 7).max().getAsInt());
+        System.out.println(Arrays.toString(IntStream.range(1, 100).toArray()));
+        System.out.println(IntStream.range(1,11).sum());
+//        Predicate<Integer> max = new Predicate<Integer>() {
+//            @Override
+//            public boolean test(Integer integer) {
+//                return integer > 5;
+//            }
+//        };
+//
+//        list.stream().filter(max).forEach(System.out::println);
 
 
     }
